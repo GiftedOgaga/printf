8cfda4b2c5015f0d0a5e71552d8268dc527957c6
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-05-21 22:33:10 -0700

    Godgift new commit

---
+++ test/main.c
@@ -0,0 +1,20 @@
+#include <limits.h>
+#include <stdio.h>
+#include "../main.h"
+
+/**
+ * main-Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	_printf("let's try to printf a simple sentence.\n");
+	printf("let's try to printf a simple sentence.\n");
+	_printf("my name is %s, with a first letter of %c ,i dont like %%\n", "Gift", 'G');
+
+	return (0);
+}
+
